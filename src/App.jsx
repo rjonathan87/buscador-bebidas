201ac5595994ef4f4015f80e01b6e88b259e4fb8
Jonathan Mendoza <rjonathan87@gmail.com>
2023-02-22 10:39:58 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Container } from "react-bootstrap"
+import Formulario from "./components/Formulario"
+import { CategoriasProvider } from "./context/CategoriasProvider"
+
+function App() {
+
+  return (
+    <>
+      <CategoriasProvider>
+        <header className="py-5">
+          <h1>Buscador de .</h1>
+        </header>
+        <Container className="mt-5">
+          <Formulario />
+        </Container>
+
+      </CategoriasProvider>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react'
+import { Button, Form, Row, Col } from 'react-bootstrap'
+import useCategorias from '../hooks/useCategorias'
+
+const Formulario = () => {
+  const [busqueda, setBusqueda] = useState({
+    nombre: '',
+    categoria: ''
+  })
+  const { categorias } = useCategorias()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    
+  }
+  
+  return (
+    <>
+      <Form
+        onsubmit={handleSubmit}
+      >
+        <Row>
+          <Col md={6}>
+            <Form.Group className='mb-3'>
+              <Form.Label
+                htmlFor='nombre'
+              >Nombre Bebida</Form.Label>
+              <Form.Control 
+                id='nombre'
+                type='text'
+                placeholder='Ej: Tequila, Vodka, etc'
+                name='nombre'
+                value={busqueda.nombre}
+                onChange={ e => setBusqueda({
+                  ...busqueda,
+                  [e.target.name]: e.target.value
+                })}
+              />
+            </Form.Group>
+          </Col>
+          <Col md={6}>
+            <Form.Group className='mb-3'>
+              <Form.Label
+                htmlFor='categoria'
+              >Categoría</Form.Label>
+              <Form.Select
+                id='categoria'
+                name='categoria'
+                value={busqueda.categoria}
+                onChange={ e => setBusqueda({
+                  ...busqueda,
+                  [e.target.name]: e.target.value
+                })}
+              >
+                <option value="">- Selecciona Categoría -</option>
+                {
+                  categorias.map( categoria => (
+                    <option
+                      key={categoria.strCategory}
+                      value={categoria.strCategory}
+                    >
+                      {categoria.strCategory}
+                    </option>
+                  ))
+                }
+              </Form.Select>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row
+          className='justify-content-end'
+        >
+          <Col md={3}>
+            <Button
+              variant='danger'
+              className='text-uppercase w-100'
+            >
+              Buscar Bebidas
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  )
+}
+
+export default Formulario
